f654b62bb304835d9fb85c1e012e34745b979d94
Andres Erbsen <andres@krutt.org>
2014-11-05 15:49:16 -0500

    support ISE 10

--- chacha20.v
+++ chacha20.v
@@ -1,4 +1,5 @@
 `default_nettype none
+`define ARRAY16(A) {A[0],A[1],A[2],A[3],A[4],A[5],A[6],A[7],A[8],A[9],A[10],A[11],A[12],A[13],A[14],A[15]}
 
 module chacha20_quarter(input wire [31:0] ai, bi, ci, di,
                         output reg [31:0] a, b, c, d);
@@ -19,44 +20,49 @@ module chacha20(input wire clock,
                 input  wire [63:0] nonce,
                 output reg done = 0,
                 output wire [511:0] out);
-    function [31:0] LE32(input [31:0] a);
+    function automatic [31:0] LE32(input [31:0] a);
         LE32 = {a[7:0], a[15:8], a[23:16], a[31:24]};
     endfunction
     localparam CONST = 128'h657870616e642033322d62797465206b; // "expand 32-byte k".encode('hex')
     wire [511:0] init ={CONST, key, LE32(index[31:0]), LE32(index[63:32]), nonce};
-
-    reg [31:0] x [0:15];
-    genvar j; generate for (j = 0; j < 16; j = j + 1) begin: gen
-        always @(posedge clock) begin
-            if (start)       x[j] <=      LE32(init[511 - 32*j -: 32]        );
-            if (i == ROUNDS) x[j] <= LE32(LE32(init[511 - 32*j -: 32]) + x[j]);
-        end
-    end endgenerate
-
     parameter ROUNDS=20;
     reg [4:0] i = ROUNDS+1; // [0, ROUNDS] => running; ROUNDS+1 => ready
+
+    reg [31:0] x [0:15];
+    wire [31:0] x_init [0:15];
+    wire [31:0] x_final [0:15];
+    genvar j; generate for (j = 0; j < 16; j = j + 1) begin: gen
+        assign x_init [j] =      LE32(init[511 - 32*j -: 32]        );
+        assign x_final[j] = LE32(LE32(init[511 - 32*j -: 32]) + x[j]);
+    end endgenerate
+
     wire v = i[0] == 0; // even => mix columns; odd => mix diagonals
-    wire [3:0]
-        q11 = 0,  q12 = v ? 4 : 5,  q13 = v ?  8 : 10,  q14 = v ? 12 : 15,
-        q21 = 1,  q22 = v ? 5 : 6,  q23 = v ?  9 : 11,  q24 = v ? 13 : 12,
-        q31 = 2,  q32 = v ? 6 : 7,  q33 = v ? 10 :  8,  q34 = v ? 14 : 13,
-        q41 = 3,  q42 = v ? 7 : 4,  q43 = v ? 11 :  9,  q44 = v ? 15 : 14;
-    chacha20_quarter q1(.ai(x[q11]), .bi(x[q12]), .ci(x[q13]), .di(x[q14]));
-    chacha20_quarter q2(.ai(x[q21]), .bi(x[q22]), .ci(x[q23]), .di(x[q24]));
-    chacha20_quarter q3(.ai(x[q31]), .bi(x[q32]), .ci(x[q33]), .di(x[q34]));
-    chacha20_quarter q4(.ai(x[q41]), .bi(x[q42]), .ci(x[q43]), .di(x[q44]));
+    wire [31:0] a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4, d1, d2, d3, d4;
+    chacha20_quarter q1(.ai(x[0]), .bi(x[v?4:5]), .ci(x[v?8:10]), .di(x[v?12:15]),
+                        .a(a1),    .b(b1),        .c(c1),         .d(d1));
+    chacha20_quarter q2(.ai(x[1]), .bi(x[v?5:6]), .ci(x[v?9:11]), .di(x[v?13:12]),
+                        .a(a2),    .b(b2),        .c(c2),         .d(d2));
+    chacha20_quarter q3(.ai(x[2]), .bi(x[v?6:7]), .ci(x[v?10:8]), .di(x[v?14:13]),
+                        .a(a3),    .b(b3),        .c(c3),         .d(d3));
+    chacha20_quarter q4(.ai(x[3]), .bi(x[v?7:4]), .ci(x[v?11:9]), .di(x[v?15:14]),
+                        .a(a4),    .b(b4),        .c(c4),         .d(d4));
     always @(posedge clock) begin
         if (i < ROUNDS) begin
-            x[q11] <= q1.a; x[q12] <= q1.b; x[q13] <= q1.c; x[q14] <= q1.d;
-            x[q21] <= q2.a; x[q22] <= q2.b; x[q23] <= q2.c; x[q24] <= q2.d;
-            x[q31] <= q3.a; x[q32] <= q3.b; x[q33] <= q3.c; x[q34] <= q3.d;
-            x[q41] <= q4.a; x[q42] <= q4.b; x[q43] <= q4.c; x[q44] <= q4.d;
+            {x[0], x[4], x[ 8], x[12]} <= {a1, v?b1:b4, v?c1:c3, v?d1:d2};
+            {x[1], x[5], x[ 9], x[13]} <= {a2, v?b2:b1, v?c2:c4, v?d2:d3};
+            {x[2], x[6], x[10], x[14]} <= {a3, v?b3:b2, v?c3:c1, v?d3:d4};
+            {x[3], x[7], x[11], x[15]} <= {a4, v?b4:b3, v?c4:c2, v?d4:d1};
         end
-        if (start) i <= 0;
+        if (start) begin
+            i <= 0;
+            `ARRAY16(x) <= `ARRAY16(x_init);
+        end
         if (i < ROUNDS+1) i <= i + 1;
-        if (i == ROUNDS)  done <= 1;
+        if (i == ROUNDS) begin
+            done <= 1;
+            `ARRAY16(x) <= `ARRAY16(x_final);
+        end
         if (done) done <= 0;
     end
-    assign out = {x[ 0], x[ 1], x[ 2], x[ 3], x[ 4], x[ 5], x[ 6], x[ 7],
-                  x[ 8], x[ 9], x[10], x[11], x[12], x[13], x[14], x[15]};
+    assign out = `ARRAY16(x);
 endmodule
